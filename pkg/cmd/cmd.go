bbecff94ecb6a181edd6c7ff4972faebc63164c0
artilugio0 <artilugio0@proton.me>
2025-06-21 11:57:30 -0300

    remove global variable

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -8,28 +8,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var requestsPerSecond float64
+const (
+	DefaultRequestsPerSecond float64 = 20.0
+)
 
-// EfinTestifierCmd represents the base command.
-var EfinTestifierCmd = &cobra.Command{
-	Use:   "efin-testifier <lua_file>",
-	Short: "Run Lua-based HTTP tests or a single HTTP request",
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		if err := testifier.Run(args[0], requestsPerSecond); err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-			os.Exit(1)
-		}
-	},
+func NewTestifierCmd(name string) *cobra.Command {
+	var requestsPerSecond float64
+
+	var testifierCmd = &cobra.Command{
+		Use:   name + " <lua_file>",
+		Short: "Run Lua-based HTTP tests or a single HTTP request",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := testifier.Run(args[0], requestsPerSecond); err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	testifierCmd.Flags().Float64VarP(
+		&requestsPerSecond,
+		"rps",
+		"r",
+		DefaultRequestsPerSecond,
+		"Maximum HTTP requests per second for tests",
+	)
+
+	return testifierCmd
 }
 
 // Execute runs the root command.
 func Execute() {
-	if err := EfinTestifierCmd.Execute(); err != nil {
+	if err := NewTestifierCmd("efin-testifier").Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	EfinTestifierCmd.Flags().Float64VarP(&requestsPerSecond, "rps", "r", 20.0, "Maximum HTTP requests per second for tests")
 }
